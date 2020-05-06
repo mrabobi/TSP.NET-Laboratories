138cca8291795474b17a281dd0d833f3f9e7b27c
BOBIRE <mraboby@gmail.com>
2020-05-06 18:06:19 +0300

    Upload lab10

---
+++ [LAB10] Asp_Miron_Robert_Andrei_Rp/ASP_Miron_Robert_Andrei_Rp/Data/ASP_Miron_Robert_Andrei_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ASP_Miron_Robert_Andrei_Rp.Models;
+
+namespace ASP_Miron_Robert_Andrei_Rp.Data
+{
+    public class ASP_Miron_Robert_Andrei_RpContext : DbContext
+    {
+        public ASP_Miron_Robert_Andrei_RpContext (DbContextOptions<ASP_Miron_Robert_Andrei_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASP_Miron_Robert_Andrei_Rp.Models.Movie> Movie { get; set; }
+    }
+}
